80193342db6015b47e1417ce0a8385d94e0daeaa
rachel <racheli2969@gmail.com>
2024-12-04 21:52:06 +0200

    3

--- MemoryManagementInDS/Program.cs
+++ MemoryManagementInDS/Program.cs
@@ -12,11 +12,18 @@ namespace MemoryManagementInDS
             Week1.MeasureDifferentTypes();
             */
 
-           /* Week2.MeasureStructMemory();
-            Week2.MeasureStringMemory();
-            Week2.TestStringBuilderMemory();
-            Week2.TestRegularStringMemory();
-            Week2.TestObjectGeneration();*/
+            /* Week2.MeasureStructMemory();
+             Week2.MeasureStringMemory();
+             Week2.TestStringBuilderMemory();
+             Week2.TestRegularStringMemory();
+             Week2.TestObjectGeneration();*/
+
+
+            int[] array = { 5, 3, 9, 1, 6 };
+            Week3.MaxCalculator maxCalculator = new Week3.MaxCalculator();
+            Action<int> maxDelegate = maxCalculator.UpdateMax; 
+            Week3.ProcessArray(array, maxDelegate);
+            Console.WriteLine($"Max of array: {maxCalculator.GetMax()}");
         }
 
     }
---
+++ MemoryManagementInDS/Week3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryManagementInDS
+{
+    internal class Week3
+    {
+
+        public static void ProcessArray(int[] array, Action<int> processor)
+        {
+            foreach (int item in array)
+            {
+                processor(item);
+            }
+        }
+
+
+        public class MaxCalculator
+        {
+            private int _max = int.MinValue;
+
+            // Saves the max value
+            public void UpdateMax(int number)
+            {
+                if (number > _max)
+                {
+                    _max = number;
+                }
+            }
+
+            //Returns the max value
+            public int GetMax()
+            {
+                return _max;
+            }
+        }
+
+    }
+}
